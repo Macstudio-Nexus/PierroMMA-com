f2d116d442d7ec0cdf3ae74429a41b01e33444ab
Craig Sampson <craigsjr15@gmail.com>
2025-10-24 20:29:48 -0400

    misc changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 export default function Home() {
   return (
-    <div className="h-screen">
+    <div className="h-screen p-8">
+      <h1></h1>
       <h1>Mixed Martial Arts</h1>
       <h2>Mixed Martial Arts</h2>
       <h3>Mixed Martial Arts</h3>
