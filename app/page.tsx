25655c9a8421a6741971d53b783f6dcf596b30d3
Craig Sampson <craigsjr15@gmail.com>
2025-11-13 13:55:52 -0500

    contact form all setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header/Header";
 import About from "./components/ui/sections/About";
 import Classes from "./components/ui/sections/Classes";
+import Contact from "./components/ui/sections/contact/Contact";
 import Hero from "./components/ui/sections/Hero";
 import Schedule from "./components/ui/sections/schedule/Schedule";
 
@@ -12,6 +13,7 @@ export default function Home() {
       <Classes />
       <Schedule />
       <About />
+      <Contact />
     </>
   );
 }
